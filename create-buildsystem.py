925a1e76211edab73414d5def410101e63ddf4a5
Konstantin Kouptsov <konstantin@kouptsov.com>
2018-01-05 23:23:13 -0500

    Minimal build system and directory structure. Initial commit.

---
+++ create-buildsystem.py
@@ -0,0 +1,58 @@
+import os, sys
+from subprocess import call, check_call, CalledProcessError
+
+"""
+  Convenience script
+"""
+
+print 'python version: ', sys.version
+
+SOURCE                 = os.path.abspath('src')
+BUILD                  = os.path.abspath('build')
+BUILD_TYPE             = 'Debug'
+MAJOR_VERSION          = '0'
+MINOR_VERSION          = '0'
+MICRO_VERSION          = '1'
+
+version                = '.'.join([MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION])
+
+"""
+do this:
+cmake -G "Visual Studio 14 2015 Win64" BUILD
+"""
+
+def ensure_dir(dir):
+	if not os.path.exists(dir):
+		os.makedirs(dir)
+
+def try_remove_file(file):
+	try:
+		os.remove(file)
+	except:
+		pass
+
+cmake_args = [
+	'cmake',
+	'-G',
+	'Visual Studio 14 2015 Win64',
+#    '-DCMAKE_BUILD_TYPE:STRING=' +  BUILD_TYPE,
+	SOURCE
+]
+
+print ''
+print BUILD,'> ',cmake_args[0],'^'
+for arg in cmake_args[1:-1]:
+        print '\t',arg,'^'
+print '\t',cmake_args[-1]
+print ''
+
+curdir = os.getcwd()
+try:
+	ensure_dir(BUILD)
+	os.chdir(BUILD)
+	try_remove_file('CMakeCache.txt')
+	check_call(cmake_args)
+	print 'done!'
+
+finally:
+	os.chdir(curdir)
