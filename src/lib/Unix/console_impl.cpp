76018039fef93d68d1f67097a754bb0fcdd2dff5
Konstantin Kouptsov <konstantin@kouptsov.com>
2018-01-09 00:17:00 -0500

    Added new files console*.{h,cpp}

---
+++ src/lib/Unix/console_impl.cpp
@@ -0,0 +1,14 @@
+/*
+* This file is part of the Term++ project, https://github.com/kkouptsov/term-plus-plus.
+*
+* Copyright (c) 2018 by Konstantin Kouptsov <konstantin@kouptsov.com>.
+*
+* Licensed under GNU Lesser General Public License 3.0 or later.
+* For the full license information, see LICENSE.txt
+* or go to https://www.gnu.org/licenses/lgpl-3.0.html
+*/
+
+
+namespace Terminal {
+
+}
---
+++ src/lib/console.h
@@ -0,0 +1,22 @@
+/*
+* This file is part of the Term++ project, https://github.com/kkouptsov/term-plus-plus.
+*
+* Copyright (c) 2018 by Konstantin Kouptsov <konstantin@kouptsov.com>.
+*
+* Licensed under GNU Lesser General Public License 3.0 or later.
+* For the full license information, see LICENSE.txt
+* or go to https://www.gnu.org/licenses/lgpl-3.0.html
+*/
+
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+namespace Terminal {
+
+class Console {
+
+};
+
+}
+
+#endif /* CONSOLE_H */
