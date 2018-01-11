1b8952b0a583149d16e36b6577407a3db069d093
Konstantin Kouptsov <konstantin@kouptsov.com>
2018-01-10 23:57:40 -0500

    Renamed LICENSE.txt --> LICENSE

--- src/loader/Windows/libload.c
+++ src/loader/Windows/libload.c
@@ -4,7 +4,7 @@
 * Copyright (c) 2018 by Konstantin Kouptsov <konstantin@kouptsov.com>.
 *
 * Licensed under GNU Lesser General Public License 3.0 or later.
-* For the full license information, see LICENSE.txt
+* For the full license information, see LICENSE
 * or go to https://www.gnu.org/licenses/lgpl-3.0.html
 */
 
