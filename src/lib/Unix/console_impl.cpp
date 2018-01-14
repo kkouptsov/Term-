7502630dd394b583b60fbd61f147a3703b43d79b
Konstantin Kouptsov <konstantin@kouptsov.com>
2018-01-14 17:15:11 -0500

    Added isatty and console more switching for Unix

--- src/lib/Unix/console_impl.cpp
+++ src/lib/Unix/console_impl.cpp
@@ -8,36 +8,93 @@
 * or go to https://www.gnu.org/licenses/lgpl-3.0.html
 */
 
+#include <termios.h>
 #include <unistd.h>
 
 #include <iostream>
 #include "console_impl.h"
+#include "except.h"
+#include "utils.h"
 
 namespace Terminal {
 
-struct Console::ConsoleImpl {
-	bool isatty();
+class Console::ConsoleImpl {
+private:
+	struct termios mode;
+	bool raw_flag;
+public:
 	ConsoleImpl();
 	~ConsoleImpl();
+	bool isatty();
+	bool is_raw_mode();
+	void set_raw_mode(bool);
 };
 
-Console::Console() : impl{std::make_unique<ConsoleImpl>()} {}
 
+Console::Console() : impl{std::make_unique<ConsoleImpl>()} {}
 Console::~Console() = default;
 
 bool Console::isatty() { return impl->isatty(); }
+bool Console::is_raw_mode() { return impl->is_raw_mode(); }
+void Console::set_raw_mode(bool flag) { return impl->set_raw_mode(flag); }
 
-Console::ConsoleImpl::ConsoleImpl() {
-	// TODO
+
+Console::ConsoleImpl::ConsoleImpl() : raw_flag{false} {
 }
+
 
 Console::ConsoleImpl::~ConsoleImpl() {
-	// TODO
 }
+
 
 bool Console::ConsoleImpl::isatty() {
-	// TODO
-	return true;
+	if (::isatty(fileno(stdin)) && ::isatty(fileno(stdout)))
+		return true;
+	return false;
+}
+
+
+bool Console::ConsoleImpl::is_raw_mode() {
+	return raw_flag;
+}
+
+
+void Console::ConsoleImpl::set_raw_mode(bool flag)
+{
+	if (flag == raw_flag)
+		return;
+	else if (flag) {
+		// switch to raw mode
+		if (tcgetattr(fileno(stdin), &mode)) {
+			throw ConsoleException(Utils::get_last_error());
+		}
+		struct termios raw = mode;
+		/*
+		   see 'man tcsetattr'
+		*/
+		raw.c_iflag &= ~(IGNBRK | BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+		raw.c_oflag &= ~OPOST;
+		raw.c_cflag &= ~(CSIZE | PARENB);
+		raw.c_cflag |= CS8;
+		raw.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
+		/*
+		   control characters: read() with 0.1 sec timeout, do not block
+		*/
+		raw.c_cc[VMIN] = 0;
+		raw.c_cc[VTIME] = 1;
+		if (tcsetattr(fileno(stdin), TCSAFLUSH, &raw)) {
+			throw ConsoleException(Utils::get_last_error());
+		}
+		raw_flag = true;
+	}
+	else {
+		// switch to cooked mode
+		if (tcsetattr(fileno(stdin), TCSAFLUSH, &mode)) {
+			throw ConsoleException(Utils::get_last_error());
+		}
+		raw_flag = false;
+	}
 }
 
 }
+
